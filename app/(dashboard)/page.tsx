835e4bed917a501a2f0ba72cb07449f428bf6de7
Stacia Benita <stacia.benita@ebram.org>
2025-07-29 22:13:53 +0800

    Init commit

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,6 +3,8 @@ import { File, PlusCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { ProductsTable } from './products-table';
 import { getProducts } from '@/lib/db';
+import { neon } from '@neondatabase/serverless';
+
 
 export default async function ProductsPage(
   props: {
@@ -16,6 +18,15 @@ export default async function ProductsPage(
     search,
     Number(offset)
   );
+
+  // async function create(formData: FormData) {
+  //   'use server';
+  //   // Connect to the Neon database
+  //   const sql = neon(`${process.env.DATABASE_URL}`);
+  //   const comment = formData.get('comment');
+  //   // Insert the comment from the form into the Postgres database
+  //   await sql('INSERT INTO comments (comment) VALUES ($1)', [comment]);
+  // }
 
   return (
     <Tabs defaultValue="all">
@@ -52,4 +63,10 @@ export default async function ProductsPage(
       </TabsContent>
     </Tabs>
   );
+  // return (
+  //   <form action={create}>
+  //     <input type="text" placeholder="write a comment" name="comment" />
+  //     <button type="submit">Submit</button>
+  //   </form>
+  // );
 }
